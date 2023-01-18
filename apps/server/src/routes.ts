443a75108bcd2cc5dd495534e45b26043b9ac53d
fellipeutaka <fellipeutaka@gmail.com>
2023-01-18 20:08:29 -0300

    feat(server): get habits of someday
    get the habits completed and to be completed on the day

--- apps/server/src/routes.ts
+++ apps/server/src/routes.ts
@@ -30,4 +30,47 @@ export async function routes(app: FastifyInstance) {
       },
     });
   });
+
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        createdAt: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findFirst({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits =
+      day?.dayHabits.map((dayHabit) => {
+        return dayHabit.habitId;
+      }) ?? null;
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
+  });
 }
