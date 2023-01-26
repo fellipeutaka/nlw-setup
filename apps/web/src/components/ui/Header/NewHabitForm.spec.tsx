b85d95350e26543e9a7731a5e385ed0bd8668fc5
fellipeutaka <fellipeutaka@gmail.com>
2023-01-26 20:42:20 -0300

    test(web): add tests to new habit form component
    tests if it's not possible to create a habit with invalid params

---
+++ apps/web/src/components/ui/Header/NewHabitForm.spec.tsx
@@ -0,0 +1,57 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { weekDays } from "@web/constants/weekDays";
+
+import { NewHabitForm } from "./NewHabitForm";
+
+// Mock for Radix UI Checkbox
+global.ResizeObserver = class ResizeObserver {
+  cb: any;
+  constructor(cb: any) {
+    this.cb = cb;
+  }
+  observe() {
+    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }]);
+  }
+  unobserve() {}
+  disconnect() {}
+};
+
+const elements = {
+  titleInput: "Exercise, sleep well, etc...",
+  titleErrorSpan: {
+    required: "Please, type your commitment",
+  },
+  weekDays,
+  weekDaysErrorSpan: {
+    required: "Please, select one day of week at least",
+  },
+  submitButton: "Confirm",
+};
+
+describe("<NewHabitForm />", () => {
+  it("should not be able to create a habit without title", async () => {
+    const { getByText, findByText } = render(<NewHabitForm />);
+    const sundayLabel = getByText(elements.weekDays[0]);
+    await userEvent.click(sundayLabel);
+    const submitButton = getByText(elements.submitButton);
+    await userEvent.click(submitButton);
+    const titleErrorSpan = await findByText(elements.titleErrorSpan.required);
+    expect(titleErrorSpan).toBeVisible();
+  });
+
+  it("should not be able to create a habit without week days", async () => {
+    const { getByPlaceholderText, getByText, findByText } = render(
+      <NewHabitForm />
+    );
+    const titleInput = getByPlaceholderText(elements.titleInput);
+    await userEvent.type(titleInput, "Sleep 8 hours");
+    const submitButton = getByText(elements.submitButton);
+    await userEvent.click(submitButton);
+    const weekDaysErrorSpan = await findByText(
+      elements.weekDaysErrorSpan.required
+    );
+    expect(weekDaysErrorSpan).toBeVisible();
+  });
+});
