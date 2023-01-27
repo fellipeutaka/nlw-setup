13d55c5bb1918c3db071198b588fae14e27e2a4f
fellipeutaka <fellipeutaka@gmail.com>
2023-01-26 22:01:08 -0300

    refactor(server): /habits POST API Route return status code 201

--- apps/server/src/routes.ts
+++ apps/server/src/routes.ts
@@ -7,13 +7,13 @@ import { today } from "./constants/today";
 import { prisma } from "./lib/prisma";
 
 export async function routes(app: FastifyInstance) {
-  app.post("/habits", async (request) => {
+  app.post("/habits", async (req, res) => {
     const createHabitBody = z.object({
       title: z.string(),
       weekDays: z.number().min(0).max(6).array().min(1).max(7),
     });
 
-    const { title, weekDays } = createHabitBody.parse(request.body);
+    const { title, weekDays } = createHabitBody.parse(req.body);
 
     await prisma.habit.create({
       data: {
@@ -28,14 +28,16 @@ export async function routes(app: FastifyInstance) {
         },
       },
     });
+
+    return res.status(201).send();
   });
 
-  app.get("/habits/day", async (request) => {
+  app.get("/habits/day", async (req) => {
     const getDayParams = z.object({
       date: z.coerce.date(),
     });
 
-    const { date } = getDayParams.parse(request.query);
+    const { date } = getDayParams.parse(req.query);
 
     const parsedDate = dayjs(date).startOf("day");
     const weekDay = parsedDate.get("day");
@@ -71,12 +73,12 @@ export async function routes(app: FastifyInstance) {
     };
   });
 
-  app.patch("/habits/:id/toggle", async (request) => {
+  app.patch("/habits/:id/toggle", async (req) => {
     const toggleHabitParams = z.object({
       id: z.string().uuid(),
     });
 
-    const { id } = toggleHabitParams.parse(request.params);
+    const { id } = toggleHabitParams.parse(req.params);
 
     let day = await prisma.day.findUnique({
       where: {
